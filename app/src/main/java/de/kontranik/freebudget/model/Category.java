71a9c5f7a23eb04d031203b9d78e0d73c01dde62
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2019-02-12 10:47:31 +0100

    neues feld: weight

--- app/src/main/java/de/kontranik/freebudget/model/Category.java
+++ app/src/main/java/de/kontranik/freebudget/model/Category.java
@@ -3,10 +3,18 @@ package de.kontranik.freebudget.model;
 public class Category {
     private long id;
     private String name;
+    private double weight;
 
     public Category(long id, String name) {
         this.id = id;
         this.name = name;
+        this.weight = 0;
+    }
+
+    public Category(long id, String name, double weight) {
+        this.id = id;
+        this.name = name;
+        this.weight = weight;
     }
 
     public Category(String name) {
@@ -27,6 +35,14 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
     }
 
     @Override
